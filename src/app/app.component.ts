2394f3c2902cfba06595ccda5ef633ab2739af85
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-09-18 18:02:06 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//$: là namespace
+import * as $ from 'jquery';
+
+// abc: là namespace
+// import * as abc from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'e13-jquery';
+export class AppComponent implements OnInit {
+  // khai báo kiểu javascript
+  editor: any;
+  ngOnInit() {
+    //innerHTML
+    $('#preview-native').html('ai la tri ky? cô đơn là gì? hạnh phúc có điều kiện?');
+    //
+    // abc('#preview-native').html('ai la tri ky? cô đơn là gì? hạnh phúc có điều kiện?');
+
+    //
+    // $('#logo').css('opacity', '0');
+    $('#button-file').on('click', (e) => {
+      // e.preventDefault(); // dùng cho <a> để ngăn nó kích hoạt
+      $('#image').trigger('click');
+    });
+  }
+
+  // nếu gặp lỗi kiểu thì để là 'any'
+  imageUrlBase64: any;
+
+  handleFileInput(files: FileList) {
+    const file: File = files.item(0);
+
+    $('#preview-native').html(file?.name);
+    const reader = new FileReader();
+
+    // đây là hàm asynchronous => có thể dùng
+    reader.onload = () => {
+      this.imageUrlBase64 = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
 }
